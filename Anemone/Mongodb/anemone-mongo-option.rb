3a45ce900359f7502a4e8b51a2e17cbf31fd9656
ikotan <ikotan.69@gmail.com>
2014-12-29 11:52:57 +0000

    create Mongodb, Sqlite3, PStore

---
+++ Anemone/Mongodb/anemone-mongo-option.rb
@@ -0,0 +1,19 @@
+require 'anemone'
+
+urls = []
+urls.push("http://www.yahoo.co.jp")
+
+opts = {
+  :storage => Anemone::Storage.MongoDB(
+    Mongo::Connection.new.db('crawler'),"documents" )
+}
+
+Anemone.crawl(urls, opts) do |anemone|
+  anemone.on_every_page do |page|
+    puts page.url
+    puts page.doc.xpath("//title/text()").to_s if page.doc
+  end
+end
+
+
+
