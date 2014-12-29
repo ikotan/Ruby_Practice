2137ab3b6955c22dcac190ee6a12693214bfd59d
ikotan <ikotan.69@gmail.com>
2014-12-29 17:00:47 +0000

    create mysql of anemone

---
+++ Anemone/MySQL/anemone-mysql.rb
@@ -0,0 +1,20 @@
+require 'anemone'
+
+urls = []
+urls.push("http://www.yahoo.co.jp")
+
+opts = {
+  :storage => Anemone::Storage.MySQL(
+    :username => 'root',
+    :password => '',
+    :database => 'anemone'
+  ),
+  :depth_limit => 0
+}
+
+Anemone.crawl(urls, opts) do |anemone|
+  anemone.on_every_page do |page|
+    puts page.url
+    puts page.doc.xpath("//title/text()").to_s if page.doc
+  end
+end
